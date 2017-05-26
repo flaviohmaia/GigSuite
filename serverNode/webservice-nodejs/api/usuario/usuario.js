895b40fa7e6563d1d2fb05ca8155829fcc7438fc
lucas7scr <luucas_09@yahoo.com.br>
2017-05-26 16:01:24 -0300

    criado os objetos 'perfil' e 'endereco' no usuario e corrigi as requisições de alteração que nao estavam sendo atualizadas na mesma hora

--- serverNode/webservice-nodejs/api/usuario/usuario.js
+++ serverNode/webservice-nodejs/api/usuario/usuario.js
@@ -2,7 +2,26 @@ const restful  = require('node-restful')
 const mongoose = restful.mongoose
 
 const usuarioSchema = new mongoose.Schema({
-    nomeUsuario: {type: String, required: true},
-    senha: {type: String, required: true}
+    usuario: {type: String, required: true},
+        senha: {type: String, required: true},
+        email: {type: String, required: true},
+        status: {type: String},
+        perfil: {
+            nome: {type: String, required: false},
+            apelido: {type: String, required: false},
+            categoria: {type: String, required: false},
+            sub_categoria: {type: String, required: false},
+            telefone: {type: String, required: false},
+            descricao: {type: String, required: false},
+            site: {type: String, required: false}
+        },
+        endereco: {
+            cep: {type: String, required: false},
+            uf: {type: String, required: false},
+            logradouro: {type: String, required: false},
+            bairro: {type: String, required: false},
+            cidade: {type: String, required: false},
+            numero: {type: String, required: false}
+        }
 })
 module.exports = restful.model('Usuario', usuarioSchema)
--- serverNode/webservice-nodejs/api/usuario/usuarioServicos.js
+++ serverNode/webservice-nodejs/api/usuario/usuarioServicos.js
@@ -1,3 +1,4 @@
 const Usuario = require('./usuario')
 Usuario.methods(['get', 'post', 'put', 'delete'])
+Usuario.updateOptions({new: true, runValidators: true})
 module.exports = Usuario
